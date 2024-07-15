50b98975f0fd09ca940be082603d083b802dd7cf
kushagra <goelkushagra2000@gmail.com>
2024-07-15 12:56:01 +0530

    rank button updates

--- src/components/Coins.js
+++ src/components/Coins.js
@@ -11,7 +11,7 @@ const Coins = (props) => {
             <div>
                 <div className='heading'>
                     <p>#</p>
-                    <p className='coin-name'>Coin</p>
+                    <p className='coin-rname'>Coin</p>
                     <p>Price</p>
                     <p>24h</p>
                     <p className='hide-mobile'>Volume</p>
